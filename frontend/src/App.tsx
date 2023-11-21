6e66d6118ecc202bcca441d089909adeb35c732e
momonong <morrischen2.9@gmail.com>
2023-11-21 21:27:06 +0800

    Update frontend on navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import SmartRVE from './components/SmartRVE';
-import DDPG from './components/DDPG';
-import COMP from './components/COMP';
-import Comp2Field from './components/Comp2Field';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // 使用 BrowserRouter
 
 function App() {
@@ -11,11 +7,11 @@ function App() {
     <Router> {/* 使用 BrowserRouter */}
       <Navbar />
       <Routes>
-        <Route path="/smart-rve" element={<SmartRVE />} />
+        {/* <Route path="/smart-rve" element={<SmartRVE />} />
         <Route path="/ddpg" element={<DDPG />} />
         <Route path="/comp" element={<COMP />} />
         <Route path="/comp2field" element={<Comp2Field />} />
-        <Route path="/" element={<SmartRVE />} /> {/* 默认路由 */}
+        <Route path="/" element={<SmartRVE />} /> 默认路由 */}
       </Routes>
     </Router>
   );
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,55 @@
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
-// const pages = ['Smart RVE', 'DDPG', 'COMP', 'Comp2Field'];
-const pages = ['Smart RVE', 'DDPG', 'COMP'];
-
 function Navbar() {
+  const [anchorEls, setAnchorEls] = useState<{ [key: string]: HTMLElement | null }>({});
+
+  const handleClick = (buttonId: string) => (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEls({ ...anchorEls, [buttonId]: event.currentTarget });
+  };  
+
+  const handleClose = (buttonId: string) => () => {
+    setAnchorEls({ ...anchorEls, [buttonId]: null });
+  };
+
+  // 假設有五個按鈕，每個按鈕有不同的菜單項目
+  const buttons = [
+    { id: 'btn_shift', 
+      label: '排班選項', 
+      menuItems: [{ label: '個人班表上傳', 
+                    link: '/personal-schedule-upload' }, 
+                  { label: '檢視所有班表', 
+                    link: '/view-all-schedules' },
+                  { label: '檢核排班後班表',
+                    link: '/verify-schedule'}] },
+    { id: 'btn_2', 
+    label: '按鈕2', 
+    menuItems: [{ label: '功能2-1', 
+                  link: '/func2-1' }, 
+                { label: '功能2-2', 
+                  link: '/func2-2' }] },
+    { id: 'btn_3', 
+    label: '按鈕3', 
+    menuItems: [{ label: '功能3-1', 
+                  link: '/func3-1' }, 
+                { label: '功能3-2', 
+                  link: '/func3-2' }] },
+  ];
+
   return (
     <AppBar position="fixed" style={{ width: '100%' }}>
       <Toolbar disableGutters>
         <img 
           src="https://static.wixstatic.com/media/37c673_8d76871adc8c49e181f4f9119139acb1~mv2.png/v1/fill/w_120,h_104,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/pic-10.png" 
           alt="Laboratory for Artificial Intelligence and Multiscale Modeling" 
-          style={{ marginLeft: '20px', marginRight: '4px', height: '25px', width: 'auto' }} 
+          style={{ marginLeft: '25px', marginRight: '4px', height: '25px', width: 'auto' }} 
         />
         <Typography
           variant="h6"
@@ -32,44 +66,28 @@ function Navbar() {
         >
           LAiMM
         </Typography>
-        <Typography
-          variant="h5"
-          noWrap
-          sx={{
-            mr: 2,
-            display: { xs: 'flex', md: 'none' },
-            flexGrow: 1,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.075rem',
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          LAiMM
-        </Typography>
         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          {pages.map((page) => (
-            <Button
-              component={Link}
-              to={`/${page.replace(' ', '-').toLowerCase()}`} // 转换 "Smart RVE" 为 "/smart-rve"
-              key={page}
-              style={{ margin: '5px', textTransform: 'none', color: 'white' }}
-            >
-              {page}
-            </Button>
-          ))}
-        </Box>
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-          {pages.map((page) => (
-            <Button
-              component={Link}
-              to={`/${page.replace(' ', '-').toLowerCase()}`} // 转换 "Smart RVE" 为 "/smart-rve"
-              key={page}
-              style={{ textTransform: 'none', color: 'white' }}
-            >
-              {page}
-            </Button>
+          {buttons.map((button) => (
+            <React.Fragment key={button.id}>
+              <Button
+                style={{ margin: '5px', textTransform: 'none', color: 'white' }}
+                onClick={handleClick(button.id)}
+              >
+                {button.label}
+              </Button>
+              <Menu
+                id={`menu-${button.id}`}
+                anchorEl={anchorEls[button.id]}
+                open={Boolean(anchorEls[button.id])}
+                onClose={handleClose(button.id)}
+              >
+                {button.menuItems.map((menuItem, index) => (
+                  <MenuItem key={index} onClick={handleClose(button.id)} component={Link} to={menuItem.link}>
+                    {menuItem.label}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </React.Fragment>
           ))}
         </Box>
       </Toolbar>
